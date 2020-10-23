60a530ca4e94c99bbb11be9995159ecbbbb33a7e
McQueen <chris.mcqueen@capgemini.com>
2020-10-23 07:07:33 +0200

    Add database Connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,6 +14,20 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+const itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: [true, "Please enter a Title"]
+    },
+    content: {
+        type: String,
+        require: [true, "Please enter a Title"]
+    }
+});
+const BlogItem = mongoose.model("BlogItem", itemSchema);
+
 
 let posts = [];
 
@@ -41,7 +56,13 @@ app.post("/compose", function (req, res) {
         title: req.body.postTitle,
         content: req.body.postBody
     };
-
+    posts.forEach((post) => {
+        const item = new BlogItem({
+            title: post.title,
+            content: post.content
+        });
+        item.save();
+    });
     posts.push(post);
 
     res.redirect("/");
